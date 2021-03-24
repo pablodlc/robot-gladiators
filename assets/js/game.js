5e3748c7fbc2b7ed8aaec8c222f3a4611a70fa25
pablo de la cruz <pablodlc@gmail.com>
2021-03-23 18:26:37 -0700

    fixed playerName input issues

--- assets/js/game.js
+++ assets/js/game.js
@@ -165,13 +165,21 @@ var shop = function() {
   }
 };
 
-/* END GAME FUNCTIONS */
+// function to set name
+var getPlayerName = function() {
+  var name = "";
 
-/* GAME INFORMATION / VARIABLES */
+  while (name === "" || name === null) {
+    name = prompt("What is your robot's name?");
+  }
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
 
 // player information
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
